6abbec02b5b90d89396b21c79bf6d09b1340e1b1
Felipe Rojas <pradofeliperojas@gmail.com>
2025-10-06 09:39:26 -0500

    Added terraform, az credentials and deployment

---
+++ infra/terraform/main.tf
@@ -0,0 +1,127 @@
+locals {
+  name = lower(replace(var.project_name, " ", "-"))
+}
+
+resource "random_password" "pg" {
+  length  = 20
+  special = true
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${local.name}-sonarqube-rg"
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${local.name}-vnet"
+  address_space       = ["10.10.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "${local.name}-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.10.1.0/24"]
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "${local.name}-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${local.name}-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefixes    = var.allow_ips
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTP-SONAR"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "9000"
+    source_address_prefixes    = var.allow_ips
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "${local.name}-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "${local.name}-sonarqube-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = var.vm_ssh_public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  custom_data = base64encode(templatefile("${path.module}/cloud-init.yaml", {
+    pg_password             = random_password.pg.result
+  }))
+
+  tags = var.tags
+}
+
+output "sonarqube_url" {
+  value       = "http://${azurerm_public_ip.pip.ip_address}:9000"
+  description = "SonarQube base URL"
+}
+
+output "ssh_command" {
+  value       = "ssh ${var.admin_username}@${azurerm_public_ip.pip.ip_address}"
+  description = "SSH command to access the VM"
+}
---
+++ infra/terraform/variables.tf
@@ -0,0 +1,40 @@
+variable "project_name" {
+  type        = string
+  default     = "aroma-life"
+  description = "Project name prefix for resources"
+}
+
+variable "location" {
+  type        = string
+  default     = "eastus"
+  description = "Azure region"
+}
+
+variable "admin_username" {
+  type        = string
+  default     = "azureuser"
+  description = "Admin username for VM"
+}
+
+variable "vm_size" {
+  type        = string
+  default     = "Standard_B2s"
+  description = "Azure VM size"
+}
+
+variable "allow_ips" {
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+  description = "Allowed source IP CIDRs for inbound access (SSH, HTTP)"
+}
+
+variable "vm_ssh_public_key" {
+  type        = string
+  description = "SSH public key for admin user"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Tags to apply to resources"
+}
